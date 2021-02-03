6a6eeb0c5f2dd45188f071844f64209f8f41cd0f
david vanmak <vanmakdavid.lwg@gmail.com>
2021-02-03 10:52:19 +0100

    feat: Install Faker

---
+++ src/DataFixtures/PropertyFixture.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Property;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class PropertyFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $property = new Property;
+            $manager->persist($property);
+        }
+        $manager->flush();
+    }
+}
