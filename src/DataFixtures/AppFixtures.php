52684a50567f9f0ed6c553b2816f83939366cf79
david vanmak <vanmakdavid.lwg@gmail.com>
2021-02-03 11:38:11 +0100

    feat: Update Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\Heat;
 use App\Entity\Property;
 use Cocur\Slugify\Slugify;
@@ -10,8 +11,15 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $em)
+    public function load(ObjectManager $manager)
     {
+
+        // ------------------------------------------------------------ \\
+        // ----------------------------------Initialize FAKER ----------\\
+        // ------------------------------------------------------------- \\        
+
+        $faker = Factory::create('fr_FR');
+
         // ------------------------------------------------------------ \\
         // ----------------------------------Creat Heat Type -----------\\
         // ------------------------------------------------------------- \\
@@ -33,48 +41,42 @@ class AppFixtures extends Fixture
             $heatTypeArray[] = $heatObject;
 
             // Persist current heat to DB
-            $em->persist($heatObject);
+            $manager->persist($heatObject);
         }
 
         // ------------------------------------------------------------ \\
         // ------------------------Creat Properties --------------------\\
         // ------------------------------------------------------------- \\
 
-        // Properties Names' Array 
-        $properties = ['Property1', 'Property2', 'Property3'];
-
-        foreach ($properties as $property) {
+        for ($i = 0; $i < mt_rand(100, 300); $i++) {
             $property = new Property;
-            $property->setTitle("Mon premier bien")
-                ->setPrice(mt_rand(80000, 580000))
-                ->setRooms(4)
-                ->setBedrooms(3)
-                ->setDescription('Une petite description')
-                ->setSurface(mt_rand(15, 400))
-                ->setFloor(4)
-                ->setAddress('40 Avenue du Rhin')
-                ->setCity('Strasbourg')
-                ->setPostaleCode(67000)
-                ->setSold(false)
 
+            $property->setTitle($faker->words(3, true))
                 // Slugs are based on Property's Title
-                ->setSlug((new Slugify())->slugify($property->getTitle()));
-
-            // Add current property to the $PropertiesArray
-            $PropertiesArray[] = $property;
+                ->setSlug((new Slugify())->slugify($property->getTitle()))
+                ->setDescription($faker->sentences(3, true))
+                ->setSurface($faker->numberBetween(20, 450))
+                ->setRooms($faker->numberBetween(2, 10))
+                ->setBedrooms($faker->numberBetween(1, 9))
+                ->setFloor($faker->numberBetween(0, 15))
+                ->setPrice($faker->numberBetween(50000, 1000000))
+                ->setCity($faker->city)
+                ->setAddress($faker->address)
+                ->setPostaleCode(((int) $faker->postcode))
+                ->setSold(false);
 
             // ------------------------------------------------------------ \\
             // ------------------------Seat Heat ------ --------------------\\
             // ------------------------------------------------------------- \\
 
-            // Select a type of heat
+            // Select a random type of heat between Electrique, Gas and Fioul
             $currentHeat = array_rand($heatTypeArray);
 
             // Assign current  property to current heat
             $heatTypeArray[$currentHeat]->addProperty($property);
 
-            $em->persist($property);
+            $manager->persist($property);
         }
-        $em->flush();
+        $manager->flush();
     }
 }
