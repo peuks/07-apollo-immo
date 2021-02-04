f568307ba2bfc206946247acb2320dfdad402351
david vanmak <vanmakdavid.lwg@gmail.com>
2021-02-04 14:45:41 +0100

    remove react

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Limenius\ReactBundle\LimeniusReactBundle::class => ['all' => true],
 ];
