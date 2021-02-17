b89020b49baf23899a5da807f339019d60dc2880
david vanmak <vanmakdavid.lwg@gmail.com>
2021-02-17 12:28:36 +0100

    fix: migrations done

---
+++ migrations/Version20210217112717.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210217112717 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE property_option');
+        $this->addSql('DROP TABLE specificity_property');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE property_option (property_id INT NOT NULL, option_id INT NOT NULL, INDEX IDX_24F16FCC549213EC (property_id), INDEX IDX_24F16FCCA7C41D6F (option_id), PRIMARY KEY(property_id, option_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE specificity_property (specificity_id INT NOT NULL, property_id INT NOT NULL, INDEX IDX_9315C96F5F69A929 (specificity_id), INDEX IDX_9315C96F549213EC (property_id), PRIMARY KEY(specificity_id, property_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE property_option ADD CONSTRAINT FK_24F16FCC549213EC FOREIGN KEY (property_id) REFERENCES property (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE property_option ADD CONSTRAINT FK_24F16FCCA7C41D6F FOREIGN KEY (option_id) REFERENCES `option` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE specificity_property ADD CONSTRAINT FK_9315C96F549213EC FOREIGN KEY (property_id) REFERENCES property (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE specificity_property ADD CONSTRAINT FK_9315C96F5F69A929 FOREIGN KEY (specificity_id) REFERENCES specificity (id) ON DELETE CASCADE');
+    }
+}
